7fedddbca325e8f2e42bfe629b01f250d8cd7aa3
Victoria Laspoumaderes <43272277+mvlaspoumaderes@users.noreply.github.com>
2020-08-19 11:48:19 -0300

    Rename Ejercicio8v2.java to Ejercicio8TP1v2.java

--- Ejercicio8TP1v2.java
+++ Ejercicio8TP1v2.java
@@ -1,7 +1,7 @@
 /*Ejercicio 8, TP1
- * Suponiendo que el primer dÌa de la semana es el Domingo, pedir un n˙mero entre 1 y 7
- * (inclusive) y mostrar el nombre del dÌa correspondiente. Si el dÌa no est· en el rango
- * permitido debe emitir por pantalla el mensaje de error "El dÌa ingresado no es v·lido".
+ * Suponiendo que el primer d√≠a de la semana es el Domingo, pedir un n√∫mero entre 1 y 7
+ * (inclusive) y mostrar el nombre del d√≠a correspondiente. Si el d√≠a no est√° en el rango
+ * permitido debe emitir por pantalla el mensaje de error "El d√≠a ingresado no es v√°lido".
  * 
  * EJEMPLO USANDO SWITCH
  */
@@ -14,7 +14,7 @@ public class Ejercicio8v2 {
 	public static void main(String[] args) {
 		// TP1EJ8
         input = new Scanner(System.in);
-        System.out.println( "Introduzca un n˙mero entre 1 y 7, inclusives" );
+        System.out.println( "Introduzca un n√∫mero entre 1 y 7, inclusives" );
         int numDia = input.nextInt();
 
 		switch (numDia) {
@@ -28,7 +28,7 @@ public class Ejercicio8v2 {
 		    System.out.println("Martes");
 		    break;
 		  case 4:
-		    System.out.println("MiÈrcoles");
+		    System.out.println("Mi√©rcoles");
 		    break;
 		  case 5:
 		    System.out.println("Jueves");
@@ -37,10 +37,10 @@ public class Ejercicio8v2 {
 		    System.out.println("Viernes");
 		    break;
 		  case 7:
-		    System.out.println("S·bado");
+		    System.out.println("S√°bado");
 		    break;
 		    default:
-		    System.out.println("El dia ingresado no es v·lido");		    
+		    System.out.println("El dia ingresado no es v√°lido");		    
 		    }
         
         input.close();		
