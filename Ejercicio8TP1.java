cf1d15eb17159f358419ab85c64ceb2a0013e4b5
Victoria Laspoumaderes <43272277+mvlaspoumaderes@users.noreply.github.com>
2020-08-19 11:47:54 -0300

    Rename Ejercicio8.java to Ejercicio8TP1.java

--- Ejercicio8TP1.java
+++ Ejercicio8TP1.java
@@ -1,7 +1,7 @@
 /*Ejercicio 8, TP1
- * Suponiendo que el primer dÌa de la semana es el Domingo, pedir un n˙mero entre 1 y 7
- * (inclusive) y mostrar el nombre del dÌa correspondiente. Si el dÌa no est· en el rango
- * permitido debe emitir por pantalla el mensaje de error "El dÌa ingresado no es v·lido".
+ * Suponiendo que el primer d√≠a de la semana es el Domingo, pedir un n√∫mero entre 1 y 7
+ * (inclusive) y mostrar el nombre del d√≠a correspondiente. Si el d√≠a no est√° en el rango
+ * permitido debe emitir por pantalla el mensaje de error "El d√≠a ingresado no es v√°lido".
  * 
  * EJEMPLO USANDO IF/ELSE IF
  */
@@ -14,7 +14,7 @@ public class Ejercicio8 {
 	public static void main(String[] args) {
 		// TP1EJ8
         input = new Scanner(System.in);
-        System.out.println( "Introduzca un n˙mero entre 1 y 7, inclusives" );
+        System.out.println( "Introduzca un n√∫mero entre 1 y 7, inclusives" );
         int numDia = input.nextInt();
         if (numDia == 1){
     		System.out.printf( "Domingo");
@@ -23,15 +23,15 @@ public class Ejercicio8 {
         } else if (numDia == 3) {
     		System.out.printf( "Martes");
         } else if (numDia == 4){
-    		System.out.printf( "MiÈrcoles");
+    		System.out.printf( "Mi√©rcoles");
         } else if (numDia == 5){
     		System.out.printf( "Jueves");
         } else if (numDia == 6) {
     		System.out.printf( "Viernes");
         } else if (numDia == 7){
-    		System.out.printf( "S·bado");
+    		System.out.printf( "S√°bado");
         } else {
-    		System.out.println( "El dia ingresado no es v·lido");
+    		System.out.println( "El dia ingresado no es v√°lido");
     	} 
          input.close();		
 	}
