63b003553d20f314edeadeb164335463cdfad23b
Victoria Laspoumaderes <43272277+mvlaspoumaderes@users.noreply.github.com>
2020-08-19 10:46:43 -0300

    Rename Ejercicio4.java to Ejercicio4TP1.java

--- Ejercicio4TP1.java
+++ Ejercicio4TP1.java
@@ -1,7 +1,7 @@
 /*Ejercicio 4, TP1
- * Realizá un programa que pida al usuario ingresar dos números enteros num1 y num2.
+ * RealizÃ¡ un programa que pida al usuario ingresar dos nÃºmeros enteros num1 y num2.
  * Luego, mostrar el resultado de la suma entre ambos, utilizando el siguiente formato: 
- * “La suma entre “ + num1 + “ y “ + num2 + “ da como resultado “ + num1 + num2
+ * â€œLa suma entre â€œ + num1 + â€œ y â€œ + num2 + â€œ da como resultado â€œ + num1 + num2
  */
 
 package ar.edu.ort.thp.tp1;
