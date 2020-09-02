0a8cf62daceed1eed65aa47db2faab4e38052dd2
Victoria Laspoumaderes <43272277+mvlaspoumaderes@users.noreply.github.com>
2020-09-02 18:45:49 -0300

    Rename EjercicioUno.java to EjercicioUnoTP2.java

--- EjercicioUnoTP2.java
+++ EjercicioUnoTP2.java
@@ -4,7 +4,7 @@ public class EjercicioUno {
 
 	public static void main(String[] args) {
 		//TP2EJ1 
-		/*Realizar un programa que muestre los números del 1 al 100 utilizando la instrucción for*/
+		/*Realizar un programa que muestre los nÃºmeros del 1 al 100 utilizando la instrucciÃ³n for*/
 		
 		int contador;
 	    for (contador = 1; contador < 101; contador++) {
