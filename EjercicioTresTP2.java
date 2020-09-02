063ce823904c4752a1ed0882e9d7818133fe97de
Victoria Laspoumaderes <43272277+mvlaspoumaderes@users.noreply.github.com>
2020-09-02 18:47:55 -0300

    Rename EjercicioTres.java to EjercicioTresTP2.java

--- EjercicioTresTP2.java
+++ EjercicioTresTP2.java
@@ -4,7 +4,7 @@ public class EjercicioTres {
 
 	public static void main(String[] args) {
 		//TP2EJ3
-		/*Realizar un programa que muestre los números del 1 al 100 utilizando la instrucción do while*/
+		/*Realizar un programa que muestre los nÃºmeros del 1 al 100 utilizando la instrucciÃ³n do while*/
 		
 		int contador;
 		contador = 0;
